e27f8eb5a56010650cee9e89309c5f4c963da853
Harry Harpham <harry@getlantern.org>
2020-12-09 19:54:47 -0700

    Add test for post-handshake garbage

--- ptlshs/conn_test.go
+++ ptlshs/conn_test.go
@@ -3,10 +3,12 @@ package ptlshs
 import (
 	"crypto/rand"
 	"crypto/tls"
+	mathrand "math/rand"
 	"net"
 	"testing"
 
 	"github.com/getlantern/tlsmasq/internal/testutil"
+	"github.com/getlantern/tlsutil"
 	"github.com/stretchr/testify/require"
 )
 
@@ -50,4 +52,56 @@ func TestHandshake(t *testing.T) {
 
 	require.NoError(t, clientConn.Handshake())
 	<-done
+}
+
+// TestPostHandshakeGarbage ensures that the connection is closed if garbage data is injected
+// between the origin's ServerFinished messaged and the server's completion signal. Otherwise, a bad
+// actor could inject such garbage data to determine whether a connection is a tlsmasq connection.
+func TestPostHandshakeGarbage(t *testing.T) {
+	var (
+		version uint16 = tls.VersionTLS12
+		suite          = tls.TLS_ECDHE_RSA_WITH_CHACHA20_POLY1305
+		secret  [52]byte
+		iv      [16]byte
+		seq     [8]byte
+
+		badSecret [52]byte
+		badIV     [16]byte
+		badSeq    [8]byte
+	)
+	for _, b := range [][]byte{secret[:], iv[:], seq[:], badSecret[:], badIV[:], badSeq[:]} {
+		_, err := rand.Read(b)
+		require.NoError(t, err)
+	}
+	clientState, err := tlsutil.NewConnectionState(version, suite, secret, iv, seq)
+	require.NoError(t, err)
+	serverState, err := tlsutil.NewConnectionState(version, suite, secret, iv, seq)
+	require.NoError(t, err)
+	badServerState, err := tlsutil.NewConnectionState(version, suite, badSecret, badIV, badSeq)
+	require.NoError(t, err)
+
+	clientTCP, serverTCP := net.Pipe()
+	client := Client(clientTCP, DialerConfig{}).(*clientConn)
+
+	serverDone := make(chan struct{})
+	go func() {
+		defer close(serverDone)
+		// Write some garbage, then the completion signal. If the garbage data is not in a TLS
+		// record, the connection may hang (as it awaits the rest of the data in the "record"). This
+		// is acceptable as far as we're concerned, but makes testing harder. Instead we send a
+		// record encrypted with a different set of parameters. The client will still get the
+		// garbage data, but not hang.
+		_, err := tlsutil.WriteRecord(serverTCP, randomData(t, mathrand.Intn(32*1024)), badServerState)
+		require.NoError(t, err)
+		require.NoError(t, err)
+		signal, err := newServerCompletionSignal()
+		require.NoError(t, err)
+		_, err = tlsutil.WriteRecord(serverTCP, *signal, serverState)
+		require.NoError(t, err)
+	}()
+
+	_, err = client.watchForCompletion(clientState)
+	require.Error(t, err)
+
+	<-serverDone
 }
