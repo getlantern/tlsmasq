b8d021c0ca5502853e4c3ed0bed54a7aec8aa131
Harry Harpham <harry@getlantern.org>
2021-04-20 17:11:55 -0600

    Add test to expose bug

--- ptlshs/conn_test.go
+++ ptlshs/conn_test.go
@@ -1,12 +1,17 @@
 package ptlshs
 
 import (
+	"bytes"
 	"crypto/rand"
 	"crypto/tls"
+	"io"
+	"io/ioutil"
 	"net"
 	"testing"
+	"time"
 
 	"github.com/getlantern/tlsmasq/internal/testutil"
+	"github.com/getlantern/tlsutil"
 	"github.com/stretchr/testify/require"
 )
 
@@ -54,4 +59,49 @@ func TestHandshake(t *testing.T) {
 
 	require.NoError(t, clientConn.Handshake())
 	<-done
+}
+
+// https://github.com/getlantern/tlsmasq/issues/17
+func TestIssue17(t *testing.T) {
+	t.Parallel()
+
+	var (
+		version          uint16 = tls.VersionTLS12
+		suite            uint16 = tls.TLS_ECDHE_ECDSA_WITH_AES_128_CBC_SHA
+		secret1, secret2 [52]byte
+		iv1, iv2         [16]byte
+		seq1, seq2       [8]byte
+	)
+	for _, b := range [][]byte{secret1[:], secret2[:], iv1[:], iv2[:], seq1[:], seq2[:]} {
+		_, err := rand.Read(b)
+		require.NoError(t, err)
+	}
+	writerState1, err := tlsutil.NewConnectionState(version, suite, secret1, iv1, seq1)
+	require.NoError(t, err)
+	writerState2, err := tlsutil.NewConnectionState(version, suite, secret2, iv2, seq2)
+	require.NoError(t, err)
+	readerState2, err := tlsutil.NewConnectionState(version, suite, secret2, iv2, seq2)
+	require.NoError(t, err)
+
+	sig, err := newClientSignal(time.Hour)
+	require.NoError(t, err)
+
+	clientTransport, serverTransport := testutil.BufferedPipe()
+	clientTransportCopy := new(bytes.Buffer)
+	clientTransportW := io.MultiWriter(clientTransport, clientTransportCopy)
+
+	_, err = tlsutil.WriteRecord(clientTransportW, []byte("pre-signal record"), writerState1)
+	require.NoError(t, err)
+	firstRecordLen := clientTransportCopy.Len()
+	_, err = tlsutil.WriteRecord(clientTransportW, *sig, writerState2)
+	require.NoError(t, err)
+
+	toOrigin, originReader := testutil.BufferedPipe()
+	go io.Copy(ioutil.Discard, originReader)
+
+	conn := &serverConn{
+		Conn:       serverTransport,
+		nonceCache: newNonceCache(time.Hour),
+	}
+	require.NoError(t, conn.watchForCompletion(firstRecordLen-1, readerState2, toOrigin))
 }
